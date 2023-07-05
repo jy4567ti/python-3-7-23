96c70f7e6e4fc0fbcf248a346eb9edb2fc3668a2
jy4567ti <jyo1234sing@gmail.com>
2023-07-05 22:53:23 +0530

    hey

---
+++ first.py
@@ -0,0 +1,95 @@
+'''print("hello")
+print("program")
+empid = 100
+print(empid)
+num1=6
+num2=4
+r=num1+num2
+print(r)
+num1=8
+num1+=3
+print(num1)
+num1=5
+num2=7
+print(num1>num2)
+print(num1>10 and num2<10)
+print(not(num1>num2))
+if False:
+    print("true")
+    n=10
+    if n>=10:
+        print("num is 10")
+        pin=190
+        if pin==100:
+            print("login")
+        else:
+            print("not login")
+
+user=input("enter username") 
+if user =="admin" :
+    password=input("enter password") 
+    if password =="admin123":
+        print("login") 
+
+    else:
+        print("not login")
+
+user=input("enter username")
+if user=="admin":
+    password=input("enter password") 
+    if password=="admin123":
+        print("login") 
+
+    else:
+        print("enter right password")
+else:
+    print("enter right user") 
+
+user=input("enter user and email")
+if(user=="admin"):
+    print("login"):
+elif user=="adin@gmail.com" :
+    print("not login")
+
+    print("1") 
+    print("2")
+    print("3")
+
+    loop while
+    12345
+    start i=1 end i<=5
+
+    i=1
+    T=4
+    while i<=10:
+        print(i*T)      
+        i+=1
+
+    i=1
+    sum1=0
+    while i<=10
+    if i%2==0:
+        sum1=sum1+i
+    i+1
+    print(sum1) 
+
+    num=int(input("ener number"))
+    num =27384
+
+    o/t 284       
+
+****'''
+for i in range(3):
+    username=input("enter the username: " )
+    password=input("enter the password: ")
+
+    if username=='admin' and password=="peeA@1233" :
+        print("access permitted")
+        break
+    else:
+        print("access denied ")
+    print("please try again")
+else:
+    print("no more attempts are available")    
+
+    
